3af15b53d9c3fd4fcd3efe6630a93b2c1a8c3f5a
JoaoVitorPerin <joao.vperin@outlook.com>
2024-01-31 23:29:26 -0300

    fix(tela inicial): refatorada a tela para exibir no mobile

--- assets/js/modal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-    $('#modalApresentacao').modal('show')
-})
-
-function abrirModalPorta(id){
-    console.log(id);
-    $(`#${id}`).modal('show')
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,101 @@
-const personagem = document.getElementById('personagem');
-const fundoTela = document.getElementById('fundoTela');
-const portaExperiencia = document.getElementById('portaExperienciaId');
-const portaCentroPokemon = document.getElementById('portaCentroPokemonId');
-const portaCertificacao = document.getElementById('portaCertificacaoId');
+const personagem = $("#personagem");
+const fundoTela = $("#fundoTela");
+const larguraTela = $(window).width();
+const alturaTela = $(window).height();
 
-let heightFundo = fundoTela.clientHeight;
-let widthFundo = fundoTela.clientWidth;
-let halfHeightFraction = heightFundo / 2.5;
-let halfWidthFraction = widthFundo / 2.5;
-
-let currentPosition = { x: 420, y: -200 };
-personagem.style.transform = `translate(${currentPosition.x}px, ${currentPosition.y}px)`;
-
-document.addEventListener('keydown', (event) => {
-    const key = event.key;
-    const step = 7;
-
-    const personagemRect = personagem.getBoundingClientRect();
-    const portaExperienciaRect = portaExperiencia.getBoundingClientRect();
-    const portaCentroPokemonRect = portaCentroPokemon.getBoundingClientRect();
-    const portaCertificacaoRect = portaCertificacao.getBoundingClientRect();
-
-    let arrayRect = [portaExperienciaRect, portaCentroPokemonRect, portaCertificacaoRect];
-    let arrayId = ['portaExperiencia', 'portaCentroPokemon', 'portaCertificacao'];
-
-    
-    function isOverDoor(portaRect){
-        const isOverlapping = !(
-            personagemRect.right < portaRect.left ||
-            personagemRect.left > portaRect.right ||
-            personagemRect.bottom < portaRect.top ||
-            personagemRect.top > portaRect.bottom
-        );
-
-        return isOverlapping
-    }
-
-    switch (key) {
-        case 'ArrowLeft':
-            if (currentPosition.x - step >= -halfWidthFraction) {
-                currentPosition.x -= step;
-                personagem.style.backgroundImage = 'url("/assets/img/characterLeft.gif")';
-            }
-            break;
-        case 'ArrowRight':
-            if (currentPosition.x + step <= halfWidthFraction) {
-                currentPosition.x += step;
-                personagem.style.backgroundImage = 'url("/assets/img/characterRight.gif")';
-            }
-            break;
-        case 'ArrowUp':
-            if (currentPosition.y - step >= -halfHeightFraction) {
-                currentPosition.y -= step;
-                personagem.style.backgroundImage = 'url("/assets/img/characterUp.gif")';
-            }
-            break;
-        case 'ArrowDown':
-            if (currentPosition.y + step <= halfHeightFraction) {
-                currentPosition.y += step;
-                personagem.style.backgroundImage = 'url("/assets/img/characterDown.gif")';
-            }
-            break;
-        case 'Enter':
-            let doorCheck;
-            console.log(arrayRect.length);
-            for (let i = 0; i < arrayRect.length; i++) {
-                console.log(personagemRect);
-                console.log(arrayRect[i]);
-                doorCheck = isOverDoor(arrayRect[i]);
-                console.log(doorCheck);
-                if(doorCheck){
-                    abrirModalPorta(arrayId[i]);
-                    break;
-                }
-            }
-            break;
-    }
-
-    personagem.style.transform = `translate(${currentPosition.x}px, ${currentPosition.y}px)`;
+$(document).ready(function () {
+    posicaoInicialPersonagem()
 });
 
-document.addEventListener('keyup', (event) => {
-    const key = event.key;
-    
-    switch (key) {
-        case 'ArrowLeft':
-            personagem.style.backgroundImage = 'url("/assets/img/character3.png")';
-            break;
-        case 'ArrowRight':
-            personagem.style.backgroundImage = 'url("/assets/img/character4.png")';
-            break;
-        case 'ArrowUp':
-            personagem.style.backgroundImage = 'url("/assets/img/character2.png")';
-            break;
-        case 'ArrowDown':
-            personagem.style.backgroundImage = 'url("/assets/img/character.png")';
-            break;
+$(document).on("keydown", function(event) {
+    const tecla = event.which;
+    validarTecla(tecla)
+});
+
+$(document).on("keyup", function(event) {
+    personagem.css({
+        'background-image': `url("/assets/img/character.png")`
+    });
+});
+
+$("#direcional").on("touchstart", function (event) {
+    touchStartX = event.originalEvent.touches[0].pageX;
+    touchStartY = event.originalEvent.touches[0].pageY;
+});
+
+$("#direcional").on("touchmove", function (event) {
+    console.log('bbbb')
+    event.preventDefault(); // Evita o comportamento padrão do scroll
+
+    const touchEndX = event.originalEvent.touches[0].pageX;
+    const touchEndY = event.originalEvent.touches[0].pageY;
+
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        deltaX > 0 ? moverPersonagem(2, 0, '/assets/img/characterRight.gif') : moverPersonagem(-2, 0, '/assets/img/characterLeft.gif');
+    } else {
+        deltaY > 0 ? moverPersonagem(0, 2, '/assets/img/characterDown.gif') : moverPersonagem(0, -2, '/assets/img/characterUp.gif');
     }
 });
+
+$("#direcional").on("touchend", function () {
+    personagem.css({
+        'background-image': `url("/assets/img/character.png")`
+    });
+});
+
+function validarTecla(tecla) {
+    switch (tecla) {
+        case 37: // seta para a esquerda
+            moverPersonagem(-10, 0, '/assets/img/characterLeft.gif');
+            break;
+        case 38: // seta para cima
+            moverPersonagem(0, -10, '/assets/img/characterUp.gif');
+            break;
+        case 39: // seta para a direita
+            moverPersonagem(10, 0, '/assets/img/characterRight.gif');
+            break;
+        case 40: // seta para baixo
+            moverPersonagem(0, 10, '/assets/img/characterDown.gif');
+            break;
+    }
+}
+
+function moverPersonagem(deltaX, deltaY, imagem) {
+    // Obter a posição atual do personagem
+    const posicaoAtual = personagem.position();
+
+    // Calcular a nova posição com base no deslocamento
+    const novaPosicao = {
+        left: posicaoAtual.left + deltaX,
+        top: posicaoAtual.top + deltaY
+    };
+
+    // Limitar a posição dentro das bordas de fundoTela
+    novaPosicao.left = Math.max(0, Math.min(larguraTela - personagem.width(), novaPosicao.left));
+    novaPosicao.top = Math.max(0, Math.min(alturaTela - personagem.height(), novaPosicao.top));
+
+    // Atualizar a posição do personagem
+    personagem.css({
+        left: novaPosicao.left + 'px',
+        top: novaPosicao.top + 'px',
+        'background-image': `url("${imagem}")`// Substitua pela URL da nova imagem
+    });
+}
+
+function posicaoInicialPersonagem() {
+    const porcentagemX = 100 - 10;
+    const porcentagemY = 20;
+
+    const x = (porcentagemX / 100) * larguraTela - personagem.width();
+    const y = (porcentagemY / 100) * alturaTela;
+
+    personagem.css({
+        position: 'absolute',
+        left: x + 'px',
+        top: y + 'px'
+    });
+}
 
